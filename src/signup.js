4bef673aa524ab6b6852db1cf8cc9a29f945fc22
kcotias <kaianexperia@gmail.com>
2019-07-04 02:29:22 -0300

    add signup page main content

--- src/signup.js
+++ src/signup.js
@@ -2,9 +2,33 @@ import React from 'react';
 
 
 const SignUp = (props) => (
-    <div>
-        <span>SignUp</span>
-    </div>
+    <form>
+        <div>
+            <label>First Name</label>
+            <input
+                type="text" name="firstName" required />
+            <label>Last Name</label>
+            <input
+                type="text" name="lastName" required />
+        </div>
+        <div>
+            <label>Email Address</label>
+            <input
+                type="email" name="email" required />
+        </div>
+        <div>
+            <label>Password</label>
+            <input
+                type="password" name="password1" />
+        </div>
+        <div>
+            <label>Re-enter Password</label>
+            <input
+                type="password" name="password2" />
+        </div>
+        <button
+            type="submit">Sign Up</button>
+    </form>
 );
 
 
