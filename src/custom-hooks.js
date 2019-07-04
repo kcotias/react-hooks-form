61ca5360c7779195d0c01a052b8657c58884e31c
kcotias <kaianexperia@gmail.com>
2019-07-04 02:36:59 -0300

    finish custom hook content

--- src/custom-hooks.js
+++ src/custom-hooks.js
@@ -1,6 +1,25 @@
-import React from 'react';
-const useSignUpForm = () => {
+import React, { useState } from 'react';
 
-    return null;
+const useSignUpForm = (callback) => {
+
+    const [inputs, setInputs] = useState({});
+
+    const handleSubmit = (event) => {
+        if (event) {
+            event.preventDefault();
+        }
+        callback();
+    }
+
+    const handleInputChange = (event) => {
+        event.persist();
+        setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+    }
+
+    return {
+        handleSubmit,
+        handleInputChange,
+        inputs
+    };
 }
 export default useSignUpForm;
